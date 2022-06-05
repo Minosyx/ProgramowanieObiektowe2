266f606cdc906f1782cd679a7c43d1952c834e08
Patryk Tajs <57626171+Minosyx@users.noreply.github.com>
2022-06-06 00:38:56 +0200

    Add files via upload

---
+++ Asynch/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynch
+{
+    class Program
+    {
+        static int x = 15;
+        static object locker = new();
+        //int initial = 0;
+        //ReaderWriterLock rwl = new ReaderWriterLock();
+        //public void myRead(object threadName)
+        //{
+        //    //Accquire Reader Lock.
+        //    rwl.AcquireReaderLock(Timeout.Infinite);
+        //    Console.WriteLine("Read start: Thread: " + threadName + " " + initial);
+        //    if (threadName.ToString() == "Thread 1")
+        //        //Irregular sleeps makes more chances of
+        //        //Multiple threads trying to access it
+        //        //at same time
+        //        Thread.Sleep(10);
+        //    else
+        //        Thread.Sleep(250);
+        //    Console.WriteLine("Read end  : Thread: " + threadName + " " + initial);
+        //    rwl.ReleaseReaderLock();
+        //    //Release Lock
+        //}
+        //public void myWrite()
+        //{
+        //    rwl.AcquireWriterLock(Timeout.Infinite);
+        //    Console.WriteLine("\nWriter start: " + initial);
+        //    initial++; //Writing
+        //    Console.WriteLine("Writer End: " + initial);
+        //    rwl.ReleaseWriterLock();
+        //    Console.WriteLine();
+        //}
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                //int[] tab = { 1, 5, 3, 6, 2, 0, -5, 6, 3 };
+                //Thread t = new Thread(Counter);
+                //Thread t1 = new Thread(Incrementer);
+                //Thread t2 = new Thread(delegate ()
+                //{
+                //    Array.Sort(tab);
+                //    foreach (var item in tab)
+                //    {
+                //        Console.Write($"tab: {item}");
+                //    }
+                //});
+                //ParameterizedThreadStart p = delegate (object o)
+                //{
+                //    Console.WriteLine(o);
+                //};
+                //Thread t3 = new Thread(p);
+                //t.Start(15);
+                //t1.Start(20);
+                //t2.Start();
+                //t3.Start(2137);
+
+                ////t.Join();
+                //Console.ReadLine();
+
+                //Thread t = new Thread(Incrementer); // locker pozwala na zakazanie dostepu do sekcji krytycznej
+                //Thread t1 = new Thread(Counter); // musi by wczesniej zdefiniowany
+                //t.Start(50);
+                //t1.Start(20);
+
+                //Program p = new();
+
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    Thread t1 = new Thread(p.myRead); //Reader Thread
+                //    //Writer Thread
+                //    Thread t2 = new Thread(new ThreadStart(p.myWrite));
+                //    //Reader Again
+                //    Thread t3 = new Thread(p.myRead);
+                //    //Start all threads
+                //    t1.Start("Thread 1");
+                //    t2.Start();
+                //    t3.Start("Thread 3");
+                //    //Wait for them to finish execution
+                //    t1.Join();
+                //    t2.Join();
+                //    t3.Join();
+                //}
+                //Console.Read();
+
+                //ThreadPool.QueueUserWorkItem(ThreadProc);
+                //Console.WriteLine("Main thread does some work, then sleeps.");
+                //Thread.Sleep(1000);
+
+                //Console.WriteLine("Main thread exits.");
+
+                //Task t = Countdown(15);
+                //Task t1 = Countdown(24);
+                //Task.WaitAll(t, t1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static async Task Countdown(int c)
+        {
+            while (c > 0)
+            {
+                Console.WriteLine(c);
+                await Task.Delay(1000);
+                c--;
+            }
+        }
+        static void ThreadProc(Object stateInfo)
+        {
+            // No state object was passed to QueueUserWorkItem, so stateInfo is null.
+            Console.WriteLine("Hello from the thread pool.");
+        }
+        public static void Incrementer(object c)
+        {
+            int i = 0;
+            while (x < (int)c)
+            {
+                //lock (locker)
+                //{
+                Monitor.Enter(locker); // takie samo działanie jak locker
+                    Console.WriteLine(x);
+                    Thread.Sleep(500);
+                    x++;
+                Monitor.Exit(locker);
+                //}
+            }
+        }
+        public static void Counter(object c)
+        {
+            int i = (int)c;
+            while (x > 0)
+            {
+                lock (locker)
+                {
+                    Console.WriteLine(x);
+                    Thread.Sleep(1000);
+                    x--;
+                }
+            }
+        }
+    }
+}
---
+++ AsynchFramework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace AsynchFramework
+{
+    public delegate string AsyncCaller(int callDuration, out int threadID);
+    public class AsyncDemo
+    {
+        public string TestMethod(int callDuration, out int threadID)
+        {
+            Console.WriteLine("Test method begins.");
+            Thread.Sleep(callDuration);
+            threadID = Thread.CurrentThread.ManagedThreadId;
+            return String.Format("My call time was {0}.", callDuration);
+        }
+    }
+    class Program
+    {
+        static int threadID;
+        static void Main(string[] args)
+        {
+            AsyncDemo ad = new AsyncDemo();
+            AsyncCaller caller = new AsyncCaller(ad.TestMethod);
+            IAsyncResult result = caller.BeginInvoke(3000, out threadID, new AsyncCallback(CallbackMethod), caller);
+            Console.WriteLine("Press Enter to close application.");
+            Console.ReadLine();
+        }
+        static void CallbackMethod(IAsyncResult ar)
+        {
+            AsyncCaller caller = (AsyncCaller)ar.AsyncState;
+            string returnValue = caller.EndInvoke(out threadID, ar);
+            Console.WriteLine("Thread {0}, return value \"{1}\".",
+            threadID, returnValue);
+        }
+    }
+}
